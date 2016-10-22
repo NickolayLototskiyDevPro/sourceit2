e79801afda65938d785946cd2669abdfda8b9d4e
Olepir Amdrew <olepir.andrew@gmail.com>
2016-10-23 00:13:35 +0300

    second task finished

--- jslesson2/alert.js
+++ jslesson2/alert.js
@@ -16,3 +16,63 @@ function year(){
 
 
 //Task 2 "calculator"
+function addition(){
+	var i = +prompt('Enter first number',0);
+    if(i===0){
+	} else if(isNaN(i)){
+		alert('Enter the number');
+	}
+    var y = +prompt('Enter second number',0);
+	if(i===0){
+	} else if(isNaN(y)){
+		alert('Enter the number');
+	}
+	else if (i + y){
+		alert('Answer ' + (y + i));
+	} 
+}
+function subtraction(){
+	var i = +prompt('Enter first number',0);
+    if(i===0){
+	} else if(isNaN(i)){
+		alert('Enter the number');
+	}
+    var y = +prompt('Enter second number',0);
+	if(i===0){
+	} else if(isNaN(y)){
+		alert('Enter the number');
+	}
+	else if (i + y){
+		alert('Answer ' + (i - y));
+	} 
+}
+function division(){
+	var i = +prompt('Enter first number',0);
+    if(i===0){
+	} else if(isNaN(i)){
+		alert('Enter the number');
+	}
+    var y = +prompt('Enter second number',0);
+	if(i===0){
+	} else if(isNaN(y)){
+		alert('Enter the number');
+	}
+	else if (y / i){
+		alert('Answer ' + (i / y));
+	} 
+}
+function multiplication(){
+	var i = +prompt('Enter first number',0);
+    if(i===0){
+	} else if(isNaN(i)){
+		alert('Enter the number');
+	}
+    var y = +prompt('Enter second number',0);
+	if(i===0){
+	} else if(isNaN(y)){
+		alert('Enter the number');
+	}
+	else if (i * y){
+		alert('Answer ' + (i * y));
+	} 
+}
