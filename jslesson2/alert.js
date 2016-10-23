c15cba065c11013137807e7f225a669bd5de5b78
Olepir Amdrew <olepir.andrew@gmail.com>
2016-10-23 10:38:59 +0300

    deleted style css

--- jslesson2/alert.js
+++ jslesson2/alert.js
@@ -1,77 +1,17 @@
 //Task 2 "Year"
 function year(){
-	var i = +prompt('Year?',0);
-	if(i === 0){
-    } else if(i === 2016){
+	var y = +prompt('Year?',0);
+	if(y === 0){
+    } else if(y === 2016){
 		alert('Right!');
-	} else if (i > 2016){
+	} else if (y > 2016){
 		alert('Wrong!  Too late.');
-	} else if (i < 2016){
+	} else if (y < 2016){
 		alert('Wrong!  Too early.');
 	} else {
-		alert('Enter this year');
+		alert('Enter the numbers');
 	}
 	
 };
 
 
-//Task 2 "calculator"
-function addition(){
-	var i = +prompt('Enter first number',0);
-    else if(isNaN(i)){
-		alert('Enter the number');
-	}
-    var y = +prompt('Enter second number',0);
-	if(i===0){
-	} else if(isNaN(y)){
-		alert('Enter the number');
-	}
-	else if (i + y){
-		alert('Answer ' + (y + i));
-	} 
-	else if ((i + y) === isNaN){
-		alert('Buzinga');
-	}
-}
-function subtraction(){
-	var i = +prompt('Enter first number',0);
-    else if(isNaN(i)){
-		alert('Enter the number');
-	}
-    var y = +prompt('Enter second number',0);
-	if(i===0){
-	} else if(isNaN(y)){
-		alert('Enter the number');
-	}
-	else if (i + y){
-		alert('Answer ' + (i - y));
-	} 
-}
-function division(){
-	var i = +prompt('Enter first number',0);
-    ielse if(isNaN(i)){
-		alert('Enter the number');
-	}
-    var y = +prompt('Enter second number',0);
-	if(i===0){
-	} else if(isNaN(y)){
-		alert('Enter the number');
-	}
-	else if (y / i){
-		alert('Answer ' + (i / y));
-	} 
-}
-function multiplication(){
-	var i = +prompt('Enter first number',0);
-    else if(isNaN(i)){
-		alert('Enter the number');
-	}
-    var y = +prompt('Enter second number',0);
-	if(i===0){
-	} else if(isNaN(y)){
-		alert('Enter the number');
-	}
-	else if (i * y){
-		alert('Answer ' + (i * y));
-	} 
-}
