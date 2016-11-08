4bf1784402d4ed4b92b9ee1c5ac729a9b391b46e
Olepir Amdrew <olepir.andrew@gmail.com>
2016-11-08 19:23:47 +0200

    Added magic:)

---
+++ tenmlate/js/main.js
@@ -0,0 +1,44 @@
+$(function() {
+	//tabs portfolio
+	$('#tabs li').click(function(){
+	   var thisClass = this.className.slice(0,2);
+	   $('div.t1').hide();
+	   $('div.t2').hide();
+	   $('div.t3').hide();
+	   $('div.' + thisClass).fadeIn(500);
+	   $('#tabs li').removeClass('active');
+	   $(this).addClass('active');
+	   return false;
+	});
+	$('li.t1').click();
+	// bx slider
+      $('.bxslider').bxSlider();
+});
+
+ 
+$(document).ready(function(){
+				// Плавный скролл по якорям
+	$('a[href^="#"]').click(function () { 
+	    elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		if($.browser.safari){
+		$('body').animate( { scrollTop: destination }, 1000 );
+		} else {
+	    	$('html').animate( { scrollTop: destination }, 1000 );
+		}
+		return false;
+		 });
+});
+
+ 
+
+
+
+
+
+
+$('.map-container')
+	.click(function(){
+			$(this).find('iframe').addClass('clicked')})
+	.mouseleave(function(){
+			$(this).find('iframe').removeClass('clicked')});
