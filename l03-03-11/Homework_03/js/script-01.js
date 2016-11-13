dae9682a100eb70b9efca5df561bf777722b3184
1980dmytroff <dmytroffva@gmail.com>
2016-11-13 19:17:03 +0200

    Homework done

--- l03-03-11/Homework_03/js/script-01.js
+++ l03-03-11/Homework_03/js/script-01.js
@@ -24,157 +24,173 @@ function solveFactorialWhile() {
 	var p = document.getElementById('stringTaskTwoWhile');
 	var n = prompt('Specify the number of factorial.', '');
 
-	function f(n) {
-		var strres = n;
-		var result = 1;
+	function f(callback) {
+		var t = callback(n);
+		if (t != undefined) {
+			var strres = t;
+			var result = 1;
 
-		while (n--) result *= n + 1;
+			while (t--) result *= t + 1;
 
-		return p.innerHTML = strres + '! = ' + result;
+			return p.innerHTML = strres + '! = ' + result;
+		}
+
+		return;
 	};
 
-	if (n === null || n == '' ) { // If it was pressed "Cancel" or sent an empty string
-		return alert('You clicked cancel or forgot to enter a numeric value. Try again!');
-	} else if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
-		n = +n;
-		if (  ((1+n)+"").indexOf(".") > 0 ) { // check that the number is an integer
-			return alert('Enter an integer!' );
-		} else if ( n < 0 ) {
-			return alert('Value must be greater than 0!' );
-		} else {
-			f(n);
-		};
-
-	} else {
-		return alert('Enter a positive integer!');
-	}
-
+	f(checkNumber);
 };
+
 
 function solveFactorialFor() {
 	var p = document.getElementById('stringTaskTwoFor');
 	var n = prompt('Specify the number of factorial.', '');
 
-	function f(n) {
-		var strres = n;
-		var result = 1;
-    
-    	for (var i = 2; i <= n; i++) {
-        	result = result * i;
-    	}
+	function f(callback) {
+		var t = callback(n);
+		if (t != undefined) {
+			var strres = t;
+			var result = 1;
 	    
-	    return p.innerHTML = strres + '! = ' + result;
+		    	for (var i = 2; i <= t; i++) {
+		        		result = result * i;
+		    	}
+		    
+		    	return p.innerHTML = strres + '! = ' + result;
+
+		}
+
+		return;
 	};
 
-	if (n === null || n == '' ) { // If it was pressed "Cancel" or sent an empty string
-		return alert('You clicked cancel or forgot to enter a numeric value. Try again!');
-	} else if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
-		n = +n;
-		if (  ((1+n)+"").indexOf(".") > 0 ) { // check that the number is an integer
-			return alert('Enter an integer!' );
-		} else if ( n < 0 ) {
-			return alert('Value must be greater than 0!' );
-		} else {
-			f(n);
-		};
-
-	} else {
-		return alert('Enter a positive integer!');
-	}
-
+	f(checkNumber);
 };
 
 function arithmProgrFor() {
 	var p = document.getElementById('stringTaskThreeFor');
 	var n = prompt('Specify the number arithmetic progression.', '');
 
-	function f(n) {
-		var s = 0;
-		for (var i = 1; i <= n; i++){
-			s += i;
+	function f(callback) {
+		var t = callback(n);
+		if (t != undefined) {
+			var s = 0;
+
+			for (var i = 1; i <= t; i++){
+				s += i;
+			}
+
+			return p.innerHTML = s;
 		}
 
-		return p.innerHTML = s;
+		return;
 	};
 
-	if (n === null || n == '' ) { // If it was pressed "Cancel" or sent an empty string
-		return alert('You clicked cancel or forgot to enter a numeric value. Try again!');
-	} else if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
-		n = +n;
-		if (  ((1+n)+"").indexOf(".") > 0 ) { // check that the number is an integer
-			return alert('Enter an integer!' );
-		} else if ( n < 0 ) {
-			return alert('Value must be greater than 0!' );
-		} else {
-			f(n);
-		};
-
-	} else {
-		return alert('Enter a positive integer!');
-	}
-
+	f(checkNumber);
 };
 
 function arithmProgrFormula() {
 	var p = document.getElementById('stringTaskThreeFormula');
 	var n = prompt('Specify the number arithmetic progression.', '');
 
-	function f(n) {
-		var result;
+	function f(callback) {
+		var t = callback(n);
+		if (t != undefined) {
+			var result;
 
-		result = n*(n+1) / 2;
+			result = t*(t+1) / 2;
 
-		return p.innerHTML = result;
+			return p.innerHTML = result;
+		}
 
+		return;
 	};
 
-	if (n === null || n == '' ) { // If it was pressed "Cancel" or sent an empty string
-		return alert('You clicked cancel or forgot to enter a numeric value. Try again!');
-	} else if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
-		n = +n;
-		if (  ((1+n)+"").indexOf(".") > 0 ) { // check that the number is an integer
-			return alert('Enter an integer!' );
-		} else if ( n < 0 ) {
-			return alert('Value must be greater than 0!' );
-		} else {
-			f(n);
-		};
-
-	} else {
-		return alert('Enter a positive integer!');
-	}
-
+	f(checkNumber);
 };
 
 function getLiter() {
 	var p = document.getElementById('stringFour');
 	var n = prompt('Enter the number of literatsy.', '');
 	var s = 0;
-	var t = 3;
+	var q = 3;
 	var a = 'a';
 	var b = 'b';
 	var c = 'c';
 	var str = '';
 
-	function f(n) {
-		if (n) {
-			do {
-				s++;
-				if (s%t == 1) {
-					str = str + a;
-				} else if (s%t == 2) {
-					str = str + b;
-				} else {
-					str = str + c;
-				}
-			} while ( str.length < n );
+	function f(callback) {
+		var t = callback(n);
+		if (t != undefined) {
+			if (t) {
+				do {
+					s++;
+					if (s%q == 1) {
+						str = str + a;
+					} else if (s%q == 2) {
+						str = str + b;
+					} else {
+						str = str + c;
+					}
+				} while ( str.length < t );
+
+				return p.innerHTML = str;
+			}
 
 			return p.innerHTML = str;
 		}
 
-		return p.innerHTML = str;
+		return;
 	};
 
+	f(checkNumber);
+};
+
+function getBinary() {
+	var p = document.getElementById('stringFive');
+	var n = prompt('Specify the number arithmetic progression.', '');
+
+	function f(callback) {
+		var t = callback(n);
+		if (t != undefined) {
+			var strres = t;
+			var t = t;
+			var str = '';
+
+			if (!t) {
+				return p.innerHTML = strres + '<sub>10</sub>' + ' = ' + strres + '<sub>2</sub>';	
+			} else if (t == 1) {
+				return p.innerHTML = strres + '<sub>10</sub>' + ' = ' + strres + '<sub>2</sub>';
+			} else  {
+				var b = t; 
+				var r;
+				do {
+					r = b; 
+					b = b / 2; 
+					if ( r%2 ) {
+						str = '1' + str;
+						b = ( r - 1) / 2;
+					} else {
+						str = '0' + str;
+					}
+				} while ( b > 1 );
+
+				str = '1' + str;
+				var l = 32 - str.length;
+				for (var i = 0; i < l; i++) {
+					str = '0' + str;
+				}
+
+				return p.innerHTML = strres + '<sub>10</sub>' + ' = ' + str + '<sub>2</sub>';
+			}
+		}
+
+		return;
+	};
+
+	f(checkNumber);
+};
+
+var checkNumber = function(n) {
 	if (n === null || n == '' ) { // If it was pressed "Cancel" or sent an empty string
 		return alert('You clicked cancel or forgot to enter a numeric value. Try again!');
 	} else if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
@@ -184,62 +200,21 @@ function getLiter() {
 		} else if ( n < 0 ) {
 			return alert('Value must be greater than 0!' );
 		} else {
-			f(n);
+			return n;
 		};
 
 	} else {
 		return alert('Enter a positive integer!');
 	}
-
 };
 
-function getBinary() {
-	var p = document.getElementById('stringFive');
-	var n = prompt('Specify the number arithmetic progression.', '');
-
-	function f(n) {
-		var strres = n;
-		var n = n;
-		var str = '';
-
-		if (!n) return p.innerHTML = strres + '<sub>10</sub>' + ' = ' + strres + '<sub>2</sub>';
-		if (n == 1) return p.innerHTML = strres + '<sub>10</sub>' + ' = ' + strres + '<sub>2</sub>';
-
-		if (n > 0 ) {
-			var b = n; 
-			var r;
-			do {
-				r = b; 
-				b = b / 2; 
-				if ( r%2 ) {
-					str = '1' + str;
-					b = ( r - 1) / 2;
-				} else {
-					str = '0' + str;
-				}
-			} while ( b > 1 );
-
-			str = '1' + str;
-
-			return p.innerHTML = strres + '<sub>10</sub>' + ' = ' + str + '<sub>2</sub>';
-		}
-
-	};
-
-	if (n === null || n == '' ) { // If it was pressed "Cancel" or sent an empty string
-		return alert('You clicked cancel or forgot to enter a numeric value. Try again!');
-	} else if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
-		n = +n;
-		if (  ((1+n)+"").indexOf(".") > 0 ) { // check that the number is an integer
-			return alert('Enter an integer!' );
-		/*} else if ( n < 0 ) {
-			return alert('Value must be greater than 0!' );*/
-		} else {
-			f(n);
-		};
-
-	} else {
-		return alert('Enter a positive integer!');
-	}
-
-};
+$(document).ready(function($) {
+	$('.tabs_menu a').click(function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		$('.tabs_menu .active').removeClass('active');
+		$(this).addClass('active');
+		$('.task').hide();
+		$('#'+$(this).attr('href')).fadeIn(400);
+	});
+});
