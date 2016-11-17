6e6917ca4d02d294df1c9a524cabf79964626d60
1980dmytroff <dmytroffva@gmail.com>
2016-11-17 17:53:46 +0200

    HW4-2

--- l04-16-11/Homework_04/js/script-01.js
+++ l04-16-11/Homework_04/js/script-01.js
@@ -1,107 +1,5 @@
 'use strict'
 
-
-
-function getInput(v) {
-	var a = document.forms[0].elements[0].value;
-	var b = document.forms[0].elements[1].value;
-	var c = document.forms[0].elements[2].value;
-
-	(v) ? comGeom1(a,b,c) : comGeom2(a,b,c);
-}
-
-function comGeom1(a,b,c) {
-	var p = document.getElementById('stringTaskOne');
-	var d = a;
-	var e = b;
-	var g = c;
-
-	function f(callback) {
-		var a = callback(d);
-		var b = callback(e);
-		var c = callback(g);
-		var result = 0;
-		if (a != undefined && b != undefined && c != undefined) {
-			if (b >= 2) {
-				do {
-					result = a;
-					a = a * b;
-				} while (a <= c);
-
-				return p.innerHTML = result;
-			}
-
-			return alert("Step should be higher than 1");
-		}
-
-		return;
-	};
-
-	f(checkNumber);
-};
-
-function comGeom2(a,b,c) {
-	var p = document.getElementById('stringTaskOne');
-	var d = a;
-	var e = b;
-	var g = c;
-
-	function f(callback) {
-		var a = callback(d);
-		var b = callback(e);
-		var c = callback(g);
-		var result = 0;
-		if (a != undefined && b != undefined && c != undefined) {
-			if (b >= 2) {
-				for (var i = 0; i < c; i++) {
-					a *= b;
-				}
-
-				return p.innerHTML = a;
-			}
-
-			return alert("Step should be higher than 1");
-		}
-
-		return;
-	};
-
-	f(checkNumber);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var checkNumber = function(n) {
-	if (n === null || n == '' ) { // If it was pressed "Cancel" or sent an empty string
-		return alert('You clicked cancel or forgot to enter a numeric value. Try again!');
-	} else if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
-		n = +n;
-		if (  ((1+n)+"").indexOf(".") > 0 ) { // check that the number is an integer
-			return alert('Enter an integer!' );
-		} else if ( n <= 0 ) {
-			return alert('Value must be greater than 0!' );
-		} else {
-			return n;
-		};
-
-	} else {
-		return alert('Enter a positive integer!');
-	}
-};
-
 $(document).ready(function($) {
 	$('.tabs_menu a').click(function(e) {
 		e.stopPropagation();
@@ -112,3 +10,95 @@ $(document).ready(function($) {
 		$('#'+$(this).attr('href')).fadeIn(400);
 	});
 });
+
+window.onload = init;
+
+function init() {
+	var com1 = document.getElementById("com1");
+	var com2 = document.getElementById("com2");
+	com1.onclick = calcExp;
+	com2.onclick = glueStr;
+}
+
+//*********************************start task1******************************
+
+function calcExp() {
+	var numbers = document.forms[0].elements;
+	var errors = document.getElementsByClassName("errorForm1");
+	var res = document.getElementById('stringTaskOne');
+	var arr = [];
+
+	for (var i = 0; i < numbers.length-1; i++) {
+		var n = numbers[i].value;
+		var n = n.replace(',', '.');
+		var parse = parseValue(n);
+
+		if (!parse) {
+			numbers[i].value = "";
+			errors[i].innerHTML = 'Введите число!';
+		} else if (parse == 10) {
+			numbers[i].value = "";
+			errors[i].innerHTML = 'Число должно быть не дробным!';
+		} else if (parse == 11) {
+			numbers[i].value = "";
+			errors[i].innerHTML = 'Число должно быть больше нуля!';
+		} else {
+			errors[i].innerHTML = 'Число принято';
+			arr.push(n);
+		}
+	}
+
+	(arr.length == 3) ? comGeom(arr) : res.innerHTML = 'empty';
+}
+
+	function parseValue(n) {
+		if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
+			n = +n;
+			if (  ((1+n)+"").indexOf(".") > 0 ) { // check that the number is an integer
+				return 10;
+			} else if ( n <= 0 ) {
+				return 11;
+			} else {
+				return true;
+			};
+		} else {
+			return false;
+		}
+	};
+
+function comGeom(arg) {
+	var errors = document.getElementsByClassName("error");
+	var res = document.getElementById('stringTaskOne');
+	var arrNum = arg;
+	var a = arrNum[0];
+	var b = arrNum[1];
+	var c = arrNum[2];
+
+	res.innerHTML = 'empty';
+
+	for (var i = 0; i < c; i++) {
+		a *= b;
+	}
+
+	return res.innerHTML = a;
+};
+
+//*******************************end task1***************************
+
+function glueStr() {
+	var string = document.forms[1].elements[0];
+	var errors = document.querySelector("span.errorForm2");
+	var res = document.getElementById('stringTaskTwo');
+	var str = string.value;
+
+	console.log(errors);
+
+	res.innerHTML = 'empty';
+
+	if (str.length) {
+		errors.innerHTML = 'Строка принята';
+		return res.innerHTML = '!' + str + '!';
+	} else {
+		errors.innerHTML = 'Введите строку';
+	}
+}
