ffab75f34a38c0e12e2a0ed69c67ba2e1b6f73a7
1980dmytroff <dmytroffva@gmail.com>
2016-11-20 22:49:32 +0200

    bugs fixed in 4 and 5 Homework

--- l05-17-11/Homework_05/js/script-01.js
+++ l05-17-11/Homework_05/js/script-01.js
@@ -19,13 +19,18 @@ function init() {
 	var com4 = document.getElementById('com4');
 	var com5 = document.getElementById('com5');
 	var com6 = document.getElementById('com6');
-
+	var com7 = document.getElementById('com7');
+	var com8 = document.getElementById('com8');
+	var com9 = document.getElementById('com9');
 
 	com2.onclick = getRev;
 	com3.onclick = getEven;
 	com4.onclick = getMatrix;
 	com5.onclick = getSumOdd;
 	com6.onclick = getMaxNum;
+	com7.onclick = getSortNum;
+	com8.onclick = isPalindrome;
+	com9.onclick = getCropStr;
 }
 
 //*******************************start task1******************************
@@ -93,7 +98,7 @@ function getMatrix() {
 	var newArr = [];
 
 	for (var i = 0; i < elements0.length; i++) {
-		var n = elements0[i].value.replace(',', '.');
+		var n = elements0[i].value.replace(',', '.');  // replace the commas on the point
 		if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
 			arr0.push(+n);
 		} else {
@@ -126,7 +131,6 @@ function getMatrix() {
 	arr.push(arr2);
 
 	var sum = 0;
-	var sumSrting ='';
 
 	for (var i = 0; i < arr.length; i++) {
 	    	for (var j = 0; j < arr.length; j++) {
@@ -162,7 +166,7 @@ function getSumOdd() {
 	}
 
 	for ( var i = 0; i < arr.length; i++) {
-		if (arr[i] & 1) {
+		if (arr[i] & 1) {       		// Odd check
 			newArr.push(arr[i]);
 		}
 	}
@@ -182,7 +186,6 @@ function getMaxNum() {
 	var res = document.getElementById('resultTaskSix');
 	var elements = document.forms.form6.elements;
 	var arr = [];
-	var maxEl;
 
 	for (var i = 0; i < elements.length; i++) {
 		var n = elements[i].value.replace(',', '.');
@@ -194,29 +197,80 @@ function getMaxNum() {
 		}
 	}
 
-	console.log(arr);
-
-	/*for ( var i = 0; i < arr.length; i++) {
-		if (arr[i] & 1) {
-			newArr.push(arr[i]);
+	var maxEl = arr[0];
+	for (var i = 0; i < arr.length; i++) {
+		if (maxEl < arr[i]) {
+			maxEl = arr[i];
 		}
 	}
 
-	for ( var i = 0; i < newArr.length; i++) {
-		if (i in newArr) sum += newArr[i];
-	}
-
-	var text = newArr.join(' + ');
-
-	return res.innerHTML = text + ' = ' +sum;*/
+	return res.innerHTML = maxEl;
 };
 
 //*******************************start task7***************************
 
+function getSortNum() {
+	var res = document.getElementById('resultTaskSeven');
+	var elements = document.forms.form7.elements;
+	var arr = [];
 
+	for (var i = 0; i < elements.length; i++) {
+		var n = elements[i].value.replace(',', '.');
+		if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
+			arr.push(+n);
+		} else {
+			elements[i].value = '';
+			return res.innerHTML = 'Элементами массива должны быть только числа';
+		}
+	}
+
+	for (var j = 0, len = arr.length - 1; j < len; j++) {
+		var swapped = false;
+		var i = 0;
+
+		while (i < len - j) {
+			if (arr[i] < arr[i + 1]) {  // used Bubble Sort
+				var c = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = c;
+				swapped = true;
+			}
+			i++;
+		}
+		if (!swapped) break;
+	}
+
+	return res.innerHTML = arr.join(', ');
+};
 
 //*******************************start task8***************************
 
+function isPalindrome() {
+	var res = document.getElementById('resultTaskEight');
+	var string = document.forms.form8.elements.palindrome.value;
+	string = string.trim();
+	var strLen = string.length;
 
+	for (var i = 0; i < strLen; i++) {
+		if (string[i] === string[strLen - 1 - i]) {          //  Compare all the elements from the beginning of the line with all the elements from the end of the line
+			continue;
+		} else {
+			return res.innerHTML = 'Слово <i>"' + string + '"</i> не является палиндромом.';
+		}
+	}
+
+	return res.innerHTML = 'Слово <i>"' + string + '"</i> является палиндромом.';
+};
 
 //*******************************start task9***************************
+
+function getCropStr() {
+	var res = document.getElementById('resultTaskNine');
+	var string = document.forms.form9.elements.cropString.value;
+
+	string = string.trim();
+	var arr = string.split(' ');    // Get an array of elements from a string
+	var newString = new Array( arr[0], arr[arr.length-1] );   // create a new array of the first and last element of the array 'arr'
+
+	return res.innerHTML = newString.join(' ');
+};
