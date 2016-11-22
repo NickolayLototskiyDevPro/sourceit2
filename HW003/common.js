138918bce40fb35cf07dd106780c2a7f15ec74b5
Kirill Stativka <ssko_17@mail.ru>
2016-11-22 22:17:46 +0200

    Done home Work Three

--- HW003/common.js
+++ HW003/common.js
@@ -6,13 +6,17 @@ function geoPro() {
 	var a1 = parseInt(prompt('Write first increment:', 1));
 	var d = parseInt(prompt('write step of progression:', 2));
 	var n = parseInt(prompt('Write limit of number:', 5));
-
-	function getAr(a1,d,n){
-		var arif = a1+d*(n-1);
-		var arifS = n*(a1 + arif)/2;	
-		return arifS;	
+	function getGeo() {
+		if (isNaN(a1) && isNan(d) && isNaN(n)) {
+			alert('write a number!'); 								// check for true number
+		} else	{
+			for (var g = 1; g < n; g++) {
+				a1*= d;
+			}
+			return a1;
+		}
 	}
-	alert("arif progression of you numbers = " + getAr(a1,d,n));
+	alert("geo progression of you numbers = " + getGeo());
 	
 }
 
@@ -25,37 +29,31 @@ function pluToString() {
 		result = symbol + '' + str + '' + symbol;
 		alert(result);
 	} else {
-		alert('not not');
+		alert('You have one chance to write right');
 	}
 }
 // calculator 
 
-function getCalkulat() {
-	var firsNum = prompt('write first number', 2);
-	var	secondNum = prompt ('write second number',5);
-
-	var n1=document.getElementById('num1').value
-	var n2=document.getElementById('num2').value
-	n1=parseFloat(n1)
-	n2=parseFloat(n2)
-	var a=document.getElementById('act').value
-	var r=0
-	if(a==1) r=n1+n2
-		if(a==2) r=n1-n2
-			if(a==3) r=n1*n2
-				if(a==4){
-					if(n2==0){
-						alert("на 0 делить нельзя")
-						document.getElementById("num2").value=""
-						document.getElementById("num2").focus()
-					}
-					else r=n1/n2
-				}
-			if(a==5) r=n1/100*n2
-				if(a==6) r=Math.pow(n1,n2)
-					if(a==7) r=Math.pow(n1,0.5)
-
-						document.getElementById("result").value=r
-				}
-
-				// not finel - this is test
+function calkulator() {	
+	var firstNum = document.getElementById('num1').value;
+	var secondNum = document.getElementById('num2').value;
+	firstNum = parseFloat(firstNum);
+	secondNum = parseFloat(secondNum);
+	var option=document.getElementById('select').value;
+	var result=0;
+	if(option==1) {
+		result=firstNum+secondNum; // plus operation
+	} else	if(option==2) {
+		result=firstNum-secondNum; // minus operation
+	}	else if(option==3) {
+		result=firstNum*secondNum; // multiple operation
+	} else if(option==4){
+		if(secondNum==0){
+			alert("null is not a true number");
+			document.getElementById("num2").value = "";
+			document.getElementById("num2").focus();
+		}
+		else result=firstNum/secondNum;
+	}
+	document.getElementById("result").value=result
+}
