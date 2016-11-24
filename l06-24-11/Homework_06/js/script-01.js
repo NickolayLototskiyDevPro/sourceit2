ed0afffd87eeaa859f872d1e0a3fe6b01557d9b9
1980dmytroff <dmytroffva@gmail.com>
2016-11-25 00:48:33 +0200

    empty

---
+++ l06-24-11/Homework_06/js/script-01.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/*$(document).ready(function($) {
+	$('.tabs_menu a').click(function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		$('.tabs_menu .active').removeClass('active');
+		$(this).addClass('active');
+		$('.task').hide();
+		$('#'+$(this).attr('href')).fadeIn(400);
+	});
+});*/
+
+window.onload = init;
+
+function init() {
+	
+}
