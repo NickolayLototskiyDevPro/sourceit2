b77ac193cf6cae3f09166622f6114af1562775f4
1980dmytroff <dmytroffva@gmail.com>
2016-12-28 18:52:44 +0200

    l12-22-11

--- l12-22-11/Homework_12/js/script-01.js
+++ l12-22-11/Homework_12/js/script-01.js
@@ -1,28 +1,30 @@
 'use strict'
 
-var vowels = [65, 69, 73, 79, 85, 89, 97, 101, 105, 111, 117, 121];
+function createTable () {
+	var res = document.getElementById('resultTaskOne');
+	var row = document.forms.table.elements.row.value;
+	var col = document.forms.table.elements.col.value;
 
-document.getElementsByTagName('input')[0].onkeypress = function(e) {
+	var table = document.createElement('table');
+	var str = '';
 
-	var chr = getChar(e);
-
-	if (chr == null) return;
-
-	for (var i = 0; i < vowels.length; i++) {
-		if (vowels[i] == chr) return false;
+	for (var i = 0; i < row; i++) {
+		if (i & 1) {
+			str+="<tr class='even'>";
+		} else {
+			str+="<tr class='odd'>";
+		}
+			for (var j = 0; j < col; j++) {
+				if (j & 1) {
+					str+="<td class='even'></td>";
+				} else {
+					str+="<td class='odd'></td>";
+				}
+			}
+		str+="</tr>"
 	}
-}
 
-function getChar(event) {
-	if (event.which == null) {
-		if (event.keyCode < 32) return null; // special key
-		return event.keyCode // IE
-	}
-
-	if (event.which != 0 && event.charCode != 0) {
-		if (event.which < 32) return null; // special key
-		return event.which // other
-	}
-
-	return null; // special key
+	table.innerHTML = str;
+	res.innerHTML = '';
+	res.appendChild(table);
 }
